6f6c322816b162a80fd57126556654da0f4d3abf
fiorexyz <angiefiorellaguzman2006@gmail.com>
2026-02-01 23:50:37 -0600

    Guia 2

---
+++ carrito-compras/src/App.jsx
@@ -0,0 +1,100 @@
+import { useEffect } from "react"
+import { Footer } from "./components/Footer"
+import { Guitar } from "./components/Guitar"
+import { Header } from "./components/Header"
+import { useState } from "react"
+import { db } from "./data/db"
+
+function App () {
+
+  function initialCart(){
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data, setData] = useState(db)
+  const [cart, setCart] = useState(initialCart)
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+
+  function addToCart(guitar){
+    const itemIndex = cart.findIndex((item) => guitar.id === item.id)
+
+    if(itemIndex === -1){
+      guitar.quantity = 1
+      setCart([...cart, guitar])
+    } else {
+      const updatedCart = [...cart]
+      updatedCart[itemIndex].quantity++
+      setCart(updatedCart)
+    }
+  }
+
+  function calculateTotal(){
+    let total = cart.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    )
+    return total
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map(item =>
+      item.id === id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    )
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map(item =>
+      item.id === id && item.quantity > 1
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    )
+    setCart(updatedCart)
+  }
+
+  function removeFromCart(id) {
+    const updatedCart = cart.filter(item => item.id !== id)
+    setCart(updatedCart)
+  }
+
+  function clearCart() {
+    setCart([])
+  }
+
+  return (
+    <>
+      <Header
+        cart={cart}
+        total={calculateTotal()}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        removeFromCart={removeFromCart}
+        clearCart={clearCart}
+      />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar
+              guitar={guitar}
+              key={guitar.id}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
